793bada906f6939ec0eab5ba7f313a4aa6457ef3
alinamkhoyan00 <alinamkhoyan19@gmail.com>
2022-07-20 21:44:21 +0400

    Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,32 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+    public static void printHourglassOfGivenSize(int height) {
+        for (int i = 0; i < height / 2; i++) {
+            for (int j = 0; j < height; j++) {
+                if (j < i || j > (height - i - 1)) {
+                    System.out.print(" ");
+                    continue;
+                } else {
+                    System.out.print("8");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = height / 2 ; i < height; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i < j || (height - i  - 1) > j) {
+                    System.out.print(" ");
+                    continue;
+                } else {
+                    System.out.print("8");
+                }
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(8);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,22 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
+    public static void printTriangle(int cathetusLength) {
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength; j++) {
+                if (j <= i) {
+                    System.out.print("8");
+                    continue;
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 
+    public static void main(String[] args) {
+        printTriangle(4);
     }
 }
+
+
